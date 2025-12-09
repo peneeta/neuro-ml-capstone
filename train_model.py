8cd87d5654618641b69a68506dbcf0a2549d9d6f
WS_Tiger ZhaoLab <you@example.com>
2025-12-08 23:06:46 -0500

    saving

--- train_model.py
+++ train_model.py
@@ -61,19 +61,19 @@ def TrainPerWell(train_img_path, val_img_path, checkpoint_dir):
 
 home = Path.home()
 
-# try B1 first
+# train B1 first
 
 # run this first
 # rsync -rltpDvp -e 'ssh -l pwojcik' data.bridges2.psc.edu:~/lm_lab_proj/for_training/B1_split .
 
-# print("Starting B1 training")
-# base_training_dir = home / "em_capstone_f25" / "Images" / "B1_split"
-# train_imgs = base_training_dir / "train_full"
-# val_imgs = base_training_dir / "val"
+print("Starting B1 training")
+base_training_dir = home / "em_capstone_f25" / "Images" / "B1_split"
+train_imgs = base_training_dir / "train_full"
+val_imgs = base_training_dir / "val"
 
-# ckpt_dir = home / "em_capstone_f25" / "checkpoint" / "B1_best_checkpoint"
+ckpt_dir = home / "em_capstone_f25" / "checkpoint" / "B1_best_checkpoint"
 
-# TrainPerWell(train_imgs, val_imgs, checkpoint_dir=ckpt_dir)
+TrainPerWell(train_imgs, val_imgs, checkpoint_dir=ckpt_dir)
 
 
 # train B2 on WS_Tiger
@@ -94,12 +94,20 @@ home = Path.home()
 # run this first
 # rsync -rltpDvp -e 'ssh -l pwojcik' data.bridges2.psc.edu:~/lm_lab_proj/for_training/B3_split .
 
-print("Starting B3 training")
+# print("Starting B3 training")
 
-base_training_dir = home / "em_capstone_f25" / "Images" / "B3_split"
-train_imgs = base_training_dir / "train_full"
-val_imgs = base_training_dir / "val"
+# base_training_dir = home / "em_capstone_f25" / "Images" / "B3_split"
+# train_imgs = base_training_dir / "train_full"
+# val_imgs = base_training_dir / "val"
 
-ckpt_dir = home / "em_capstone_f25" / "checkpoint" / "B3_best_checkpoint"
+# ckpt_dir = home / "em_capstone_f25" / "checkpoint" / "B3_best_checkpoint"
 
-TrainPerWell(train_imgs, val_imgs, checkpoint_dir=ckpt_dir)
+# TrainPerWell(train_imgs, val_imgs, checkpoint_dir=ckpt_dir)
+
+#############################################################################
+
+# bash commands to get test images
+# rsync -rltpDvp -e 'ssh -l pwojcik' data.bridges2.psc.edu:~/lm_lab_proj/preprocessed/B1_preprocessed/test ./B1_test
+# rsync -rltpDvp -e 'ssh -l pwojcik' data.bridges2.psc.edu:~/lm_lab_proj/preprocessed/B2_preprocessed/test ./B2_test
+# rsync -rltpDvp -e 'ssh -l pwojcik' data.bridges2.psc.edu:~/lm_lab_proj/preprocessed/B3_preprocessed/test ./B3_test
+
